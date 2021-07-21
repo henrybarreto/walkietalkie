bd2c550bed7d33d78ac1f38ff28d219bcd5c150d
Henry Barreto <contato@henrybarreto.dev>
2021-07-21 01:24:54 -0300

    Refactoring soldier to a functional approach

--- src/soldier.rs
+++ src/soldier.rs
@@ -6,25 +6,29 @@ use soldier_config::SoldierConfig;
 
 use crate::commander::command::Command;
 use crate::reporter::report::Report;
+use std::process::Output;
 
 pub mod soldier_config;
 
 /// Represents the methods needed to execute operation on client side
 pub struct Soldier;
+
 impl Soldier {
     /**
-       Loading a configuration file called "config.ron" containing a representation of SoldierConfig.
+    Loading a configuration file called "config.ron" containing a representation of SoldierConfig.
 
-       It's panic if the file could not be open, if the file does not exists, if the content was
-       not a SoldierConfig structure or it could not be deserialized.
+    It's panic if the file could not be open, if the file does not exists, if the content was
+    not a SoldierConfig structure or it could not be deserialized.
 
-    */
-    pub fn config() -> SoldierConfig {
-        let config_file = if let Ok(config_file) = File::open(Path::new("config.ron")) {
+     */
+    fn load_config_file(path_config_file: String) -> File {
+        if let Ok(config_file) = File::open(Path::new(&path_config_file)) {
             config_file
         } else {
             panic!("Could not read the config.ron file");
-        };
+        }
+    }
+    fn convert_config_to_struct(config_file: File) -> SoldierConfig {
         match ron::de::from_reader(config_file) {
             Ok(commander_config) => commander_config,
             Err(error) => {
@@ -35,31 +39,38 @@ impl Soldier {
             }
         }
     }
+    pub fn config() -> SoldierConfig {
+        Soldier::convert_config_to_struct(
+            Soldier::load_config_file("config.ron".to_string())
+        )
+    }
+
+    fn run_command(command: Command) -> Output {
+        match std::process::Command::new(command.name)
+            .args(command.args)
+            .output() {
+            Ok(output_from_command) => { output_from_command }
+            Err(_) => { panic!("Could not execute a command") }
+        }
+    }
+
+    fn create_report_from_output(output_from_command: Output) -> Report {
+        Report {
+            status: output_from_command.status.code().unwrap(),
+            stdout: output_from_command.stdout,
+            stderr: output_from_command.stderr,
+        }
+    }
 
     /// Run a command and return a Report with the result.
     pub fn run_commands(commands: Vec<Command>) -> Vec<Report> {
         info!("Running commands");
-        let mut list_responses_from_commands: Vec<Report> = vec![];
-        for command in commands {
-            info!("Trying to executing the commands");
-            let handle = if let Ok(output_from_a_command) = std::process::Command::new(command.name)
-                .args(command.args)
-                .output()
-            {
-                info!("Command executed!");
-                output_from_a_command
-            } else {
-                warn!("A command could not be performed");
-                break;
-            };
-            let response_from_command = Report {
-                status: handle.status.code().unwrap(),
-                stdout: handle.stdout,
-                stderr: handle.stderr,
-            };
-            //info!("{:#?}", response_from_command);
-            list_responses_from_commands.push(response_from_command);
-        }
-        list_responses_from_commands
+        commands.into_iter()
+            .map(|command| {
+                info!("Trying to executing the commands");
+                Soldier::create_report_from_output(
+                    Soldier::run_command(command.clone())
+                )
+            }).collect()
     }
 }
